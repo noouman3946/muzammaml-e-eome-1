9b0335ca7cd07a5878d0404485c8c21f4549712b
noouman3946 <148061162+noouman3946@users.noreply.github.com>
2024-09-12 11:38:33 +0500

    Build

--- src/app/(pages)/profile/layout.jsx
+++ src/app/(pages)/profile/layout.jsx
@@ -1,6 +1,6 @@
 // components/ProfileSidebar.js
 import Link from 'next/link';
-import { FaWallet, FaGift, FaShoppingCart, FaUser, FaHome, FaCreditCard, FaPhone, FaShareAlt, FaSignOutAlt } from 'react-icons/fa';
+// import { FaWallet, FaGift, FaShoppingCart, FaUser, FaHome, FaCreditCard, FaPhone, FaShareAlt, FaSignOutAlt } from 'react-icons/fa';
 
 const ProfileSidebar = () => {
   return (
@@ -18,55 +18,55 @@ const ProfileSidebar = () => {
           <ul>
             <li>
               <Link href="/profile/Mywallet" className="flex items-center py-2 px-4 hover:bg-gray-100 rounded-md transition-colors">
-                <FaWallet className="mr-3 text-xl text-blue-600" />
+                {/* <FaWallet className="mr-3 text-xl text-blue-600" /> */}
                 <span>My Wallet</span>
               </Link>
             </li>
             <li>
               <Link href="/profile/Myreward" className="flex items-center py-2 px-4 hover:bg-gray-100 rounded-md transition-colors">
-                <FaGift className="mr-3 text-xl text-green-600" />
+                {/* <FaGift className="mr-3 text-xl text-green-600" /> */}
                 <span>My Reward</span>
               </Link>
             </li>
             <li>
               <Link href="/profile/Myorders" className="flex items-center py-2 px-4 hover:bg-gray-100 rounded-md transition-colors">
-                <FaShoppingCart className="mr-3 text-xl text-yellow-600" />
+                {/* <FaShoppingCart className="mr-3 text-xl text-yellow-600" /> */}
                 <span>My Order</span>
               </Link>
             </li>
             <li>
               <Link href="/profile/Personalinformation" className="flex items-center py-2 px-4 hover:bg-gray-100 rounded-md transition-colors">
-                <FaUser className="mr-3 text-xl text-purple-600" />
+                {/* <FaUser className="mr-3 text-xl text-purple-600" /> */}
                 <span>Personal Information</span>
               </Link>
             </li>
             <li>
               <Link href="/profile/Adresses" className="flex items-center py-2 px-4 hover:bg-gray-100 rounded-md transition-colors">
-                <FaHome className="mr-3 text-xl text-teal-600" />
+                {/* <FaHome className="mr-3 text-xl text-teal-600" /> */}
                 <span>Addresses</span>
               </Link>
             </li>
             <li>
               <Link href="/profile/Paymentmethods" className="flex items-center py-2 px-4 hover:bg-gray-100 rounded-md transition-colors">
-                <FaCreditCard className="mr-3 text-xl text-indigo-600" />
+                {/* <FaCreditCard className="mr-3 text-xl text-indigo-600" /> */}
                 <span>Payment Method</span>
               </Link>
             </li>
             <li>
               <Link href="/profile/Contactpreference" className="flex items-center py-2 px-4 hover:bg-gray-100 rounded-md transition-colors">
-                <FaPhone className="mr-3 text-xl text-orange-600" />
+                {/* <FaPhone className="mr-3 text-xl text-orange-600" /> */}
                 <span>Contact Preferences</span>
               </Link>
             </li>
             <li>
               <Link href="/profile/Socialnetworks" className="flex items-center py-2 px-4 hover:bg-gray-100 rounded-md transition-colors">
-                <FaShareAlt className="mr-3 text-xl text-red-600" />
+                {/* <FaShareAlt className="mr-3 text-xl text-red-600" /> */}
                 <span>Social Network</span>
               </Link>
             </li>
             <li>
               <Link href="/profile/Logout" className="flex items-center py-2 px-4 hover:bg-gray-100 rounded-md transition-colors">
-                <FaSignOutAlt className="mr-3 text-xl text-gray-600" />
+                {/* <FaSignOutAlt className="mr-3 text-xl text-gray-600" /> */}
                 <span>Logout</span>
               </Link>
             </li>
